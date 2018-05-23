e3fa791c30160b6d1e9a0fe9e9acb66e64939734
Lindsay Cox <lindsaygcox@live.co.uk>
2018-05-23 22:05:16 +0100

    First commit

---
+++ UnityProj/UnityProj/Assets/Source/IKControl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class IKControl : MonoBehaviour
+{
+    [SerializeField]
+    private Animator _animator;
+
+    [SerializeField]
+    private bool _isIKActive = false;
+
+    [SerializeField]
+    private Transform _lookObj;
+
+    private void OnAnimatorIK(int layerIndex)
+    {
+        if (_animator != null)
+        {
+            if (_isIKActive)
+            {
+                if (_isIKActive)
+                {
+                    if (_lookObj != null)
+                    {
+                        _animator.SetLookAtWeight(1);
+                        _animator.SetLookAtPosition(_lookObj.position);
+                    }
+                }
+            }
+            else
+            {
+                _animator.SetLookAtWeight(0);
+            }
+        }
+    }
+}
